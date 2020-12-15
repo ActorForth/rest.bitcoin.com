50d7c785bbf2c5d381f134a0f78f8574c35d5a69
naach <napat_pat3@hotmail.com>
2020-12-15 20:06:02 +0700

    feat: add regtest to slp API

--- src/routes/v2/slp.ts
+++ src/routes/v2/slp.ts
@@ -34,9 +34,13 @@ const SLPSDK: any = require("slp-sdk")
 const SLP: any = new SLPSDK()
 const slp: any = SLP.slpjs
 const utils: any = slp.Utils
+const BCHJS = require('bch-js-reg')
+const bchjs = new BCHJS()
 
 // TODO: Use SLP Java Indexer when set and available
 const slpDataService: ISlpData = process.env.USE_SLPDB ? new Slpdb() : new Slpdb()
+
+const isRegtest = (process.env.NETWORK == 'regtest');
 
 // Used to convert error messages to strings, to safely pass to users.
 util.inspect.defaultOptions = { depth: 5 }
@@ -279,10 +283,12 @@ async function listSingleToken(
     const s: string = JSON.stringify(query)
     const b64: string = Buffer.from(s).toString("base64")
     const url: string = `${process.env.SLPDB_URL}q/${b64}`
+    console.log('URL', url)
 
     const options = generateCredentials()
 
     const tokenRes: AxiosResponse = await axios.get(url, options)
+    console.log('TOKENRES', tokenRes)
 
     let token
     if (tokenRes.data.t.length) {
@@ -328,6 +334,7 @@ async function listBulkToken(
   next: express.NextFunction
 ): Promise<express.Response> {
   try {
+    console.log("listBulkToken")
     const tokenIds: string[] = req.body.tokenIds
 
     // Reject if tokenIds is not an array.
@@ -377,10 +384,12 @@ async function listBulkToken(
     const s: string = JSON.stringify(query)
     const b64: string = Buffer.from(s).toString("base64")
     const url: string = `${process.env.SLPDB_URL}q/${b64}`
+    console.log('URL', url)
 
     const options = generateCredentials()
 
     const tokenRes: AxiosResponse = await axios.get(url, options)
+    console.log('TOKENRES', tokenRes)
 
     const formattedTokens: any[] = []
     const txids: string[] = []
@@ -2179,7 +2188,7 @@ const processInputs = (tx: TransactionInterface): any => {
         const address: string = vin.addr
         if (address) {
           vin.legacyAddress = bitbox.Address.toLegacyAddress(address)
-          vin.cashAddress = bitbox.Address.toCashAddress(address)
+          vin.cashAddress = bitbox.Address.toCashAddress(address, true, isRegtest)
           delete vin.addr
         }
         delete vin.valueSat
@@ -2198,7 +2207,7 @@ const processInputs = (tx: TransactionInterface): any => {
         if (vout.scriptPubKey.addresses) {
           const cashAddrs: string[] = []
           vout.scriptPubKey.addresses.forEach((addr: any) => {
-            const cashAddr = bitbox.Address.toCashAddress(addr)
+            const cashAddr = bitbox.Address.toCashAddress(addr, true, isRegtest)
             cashAddrs.push(cashAddr)
           })
           vout.scriptPubKey.cashAddrs = cashAddrs
